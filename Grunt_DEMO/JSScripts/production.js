4f28adbe0efbee687a4752b3763b80545f7eb252
Gourav Sharma <gourav.sharma@ampf.com>
2015-01-23 16:11:41 -0600

    updated grunt

---
+++ Grunt_DEMO/JSScripts/production.js
@@ -0,0 +1,8 @@
+function GetVersionandText()
+{
+    return "This is version 2.0.0 for java script";
+}
+function GetVersionandText()
+{
+    return "This is version 1.0.0 for java script";
+}
--- Grunt_DEMO/gruntfile.js
+++ Grunt_DEMO/gruntfile.js
@@ -6,7 +6,7 @@ var mm = today.getMinutes() ; //January is 0!
 var ss = today.getSeconds() ;
 var today = "_"+hh + mm + ss ;
 
-
+/*
 module.exports = function (grunt) {
 
     // Configure `curl` with URLs
@@ -29,7 +29,7 @@ module.exports = function (grunt) {
             }
         }
     });
-    */
+    
 
     curl: {
         'GetArtifactory': {
@@ -45,4 +45,31 @@ module.exports = function (grunt) {
 
     // Load in `grunt-curl`
     grunt.loadNpmTasks('grunt-curl');
+    };
+    */
+
+module.exports = function (grunt) {
+
+    // 1. All configuration goes here 
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        concat: {
+            dist: {
+                src: [
+                    'dev/*.js' // All JS in the libs folder
+                    //'dev/global.js'  // This specific file
+                ],
+                dest: 'JSScripts/production.js',
+            }
+        }
+
+    });
+
+    // 3. Where we tell Grunt we plan to use this plug-in.
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
+    grunt.registerTask('default', ['concat']);
+
 };
